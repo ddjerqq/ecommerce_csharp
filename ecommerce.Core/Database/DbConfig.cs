81142a91698134f411b8ceb30464d685c30b0f77
Giorgi Nachkebia <gio2004n@gmail.com>
2022-05-11 10:37:41 +0400

    added dbconfig

---
+++ ecommerce.Core/Database/DbConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace ecommerce.Core.Database
+{
+    public class DbConfig
+    {
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ ecommerce.Core/Validators/UserCreateDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using ecommerce.Core.Models;
+using FluentValidation;
+
+namespace ecommerce.Core.Validators
+{
+    public class UserCreateDtoValidator : AbstractValidator<UserCreateDto>
+    {
+        public UserCreateDtoValidator()
+        {
+            RuleFor(x => x.Username)
+                .Matches("^[a-zA-Z0-9._]*$")
+                .WithMessage("Username can only contain letters, numbers, dots and underscores");
+        }
+    }
+}
